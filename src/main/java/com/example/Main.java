515ad3d7476bec56c957485580ff255bd7b86543
sudhanshu3721 <sudhanshug9876@gmail.com>
2025-05-27 05:08:01 +0000

    test

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Starting Spring Boot Application...");
+		System.out.println("Sudhanshu Gupta");
 		SpringApplication.run(Main.class, args);
 	}
 
